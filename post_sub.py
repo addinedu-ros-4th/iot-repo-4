c35b79bea113ec713746514d60a83aa8e431b2b3
sna145815 <sna145815@gmail.com>
2024-03-08 16:49:17 +0900

    nofound 구현 완료

--- post_sub.py
+++ post_sub.py
@@ -6,9 +6,14 @@ from PyQt5 import uic
 from qt_material import apply_stylesheet
 import mysql.connector
 from datetime import datetime
-
+from PyQt5.QtCore import QTimer,QObject, pyqtSignal
 from_class = uic.loadUiType("sub2.ui")[0]
 
+class Signal(QObject):
+    timer_start_signal = pyqtSignal()
+    timer_stop_signal = pyqtSignal()
+    show_mail_signal = pyqtSignal()
+    
 class WindowClass(QMainWindow, from_class):
     def __init__(self):
         super().__init__()
@@ -48,9 +53,40 @@ class WindowClass(QMainWindow, from_class):
 
         # 루프 시작 (메시지 수신 대기)
         self.client.loop_start()
+        
+        # 타이머 설정
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.timer_timeout)
+        # 타이머 상태를 나타내는 변수
+        self.timer_active = False
 
+        # 시그널 객체 생성
+        self.signal = Signal()
+        self.signal.timer_start_signal.connect(self.timer_start)
+        self.signal.timer_stop_signal.connect(self.timer_stop)
+        self.signal.show_mail_signal.connect(self.show_mail_message)
 
+    def timer_start(self):
+        self.timer_count = 0
+        self.timer.start(1000)
 
+    def timer_stop(self):
+        self.timer.stop()
+
+    def timer_timeout(self):
+        # 타이머가 만료될 때마다 실행되는 함수
+        self.timer_count += 1
+        #print(self.timer_count)
+        if self.timer_count >= self.timer_count %60 == 0:  # 타이머 값이 60초가 지나면
+            self.signal.show_mail_signal.emit()
+
+    def show_mail_message(self):
+        current_text = self.text.text()
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        text = current_text + '\n' + formatted_time + '\t' + '우편물 찾아가세요!'
+        self.text.setText(text)
+        
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
             print("Connected to the broker")
@@ -68,6 +104,8 @@ class WindowClass(QMainWindow, from_class):
         if msg.topic == 'post/door':
             if msg.payload.decode() == 'door is open.':
                 content = 'OPEN'
+                self.signal.timer_stop_signal.emit()
+                self.timer_active = False
             else:
                 content = 'CLOSE'
             self.insert_log(content)
@@ -77,9 +115,14 @@ class WindowClass(QMainWindow, from_class):
         elif msg.topic == 'post/sensor':
             content = 'INPUT'
             self.insert_log(content)
+            if self.timer_active == False:
+                self.signal.timer_start_signal.emit()
+                self.timer_active = True
             
         self.addText(msg)
 
+    
+            
     def insert_log(self,content):
         query = "INSERT INTO LOG_TABLE (EVENT_TIME, CONTENT) VALUES (%s, %s)"
         cur = self.connection.cursor()
@@ -104,7 +147,10 @@ class WindowClass(QMainWindow, from_class):
                 text = current_text+'\n'+formatted_time + '\t' + '문열림 !!'
                 self.text.setText(text)
         elif msg.topic == 'post/doorstate':
-            self.text.setText(input_text)
+            current_text = self.text.text()
+            text = current_text+'\n'+formatted_time + '\t' + input_text
+            self.text.setText(text)
+
 
 
     def clearText(self):
